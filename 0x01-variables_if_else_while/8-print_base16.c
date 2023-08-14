867a9e91894e393a0719886289a499eb2252daff
oti-favour <favourgraceoti@gmail.com>
2023-08-14 09:13:17 -0700

    go

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,16 +7,19 @@
  */
 int main(void)
 {
+	unsigned char a = '0';
 	int i;
 
-	for (i = 10; i < 20; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar((i % 10) + '0');
-		if (i != 19)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(a);
+		a++;
+	}
+	a = '1';
+	for (i = 0; i < 6; i++)
+	{
+		putchar('0' + a);
+		a++;
 	}
 	putchar('\n');
 	return (0);
