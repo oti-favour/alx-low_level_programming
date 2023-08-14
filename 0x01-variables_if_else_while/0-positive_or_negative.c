444698017191b95de20dd75fa38b4b685530b94a
oti-favour <favourgraceoti@gmail.com>
2023-08-14 08:19:08 -0700

    go

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,13 +11,10 @@ int main(void)
 {
     int n;
 
-    // Seed the random number generator with the current time
     srand(time(0));
 
-    // Generate a random number between -RAND_MAX/2 and RAND_MAX/2
     n = rand() - RAND_MAX / 2;
 
-    // Check and print whether the number is positive, zero, or negative
     if (n > 0) {
         printf("%d is positive\n", n);
     } else if (n == 0) {
