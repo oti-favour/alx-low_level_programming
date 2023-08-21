d92880ec486e72e2d20feb9d2d5a6c26064c72bd
oti-favour <favourgraceoti@gmail.com>
2023-08-21 07:31:33 -0700

    initial

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* Prototype for _putchar function */
+int _putchar(char c);
+
+/* Function prototypes */
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+/* Add more function prototypes as needed */
+
+#endif
