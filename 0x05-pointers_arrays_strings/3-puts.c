12051c7725e36f3588c73e2825a980fee0749a30
oti-favour <favourgraceoti@gmail.com>
2023-08-22 08:34:06 -0700

    holr

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: The string to be printed.
+ */
 void _puts(char *str)
 {
 while (*str != '\0')
