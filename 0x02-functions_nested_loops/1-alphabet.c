2fb35289b7b02c8bf7e5880c89ca7032ab411501
oti-favour <favourgraceoti@gmail.com>
2023-08-22 04:36:50 -0700

    hol

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet - Prints the lowercase alphabet followed by a new line
  */
 void print_alphabet(void)
 {
